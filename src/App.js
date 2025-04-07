f6613336edc4aca644dbd1f349cd9ee608fd9b5d
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-04-08 01:20:20 +0530

    Create Contactus page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
