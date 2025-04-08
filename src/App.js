42282b4b30cb60806f5c709b135c4044ae2f11cb
Heshani833 <heshanishehana123@gmail.com>
2025-04-08 21:33:06 +0530

    Courses page create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Contact from './pages/Contact';
-import Signup from './pages/Signup';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import Signup from "./pages/Signup";
+import Courses from "./pages/Courses";
+import "./App.css";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/courses" element={<Courses />} />
       </Routes>
     </Router>
   );
